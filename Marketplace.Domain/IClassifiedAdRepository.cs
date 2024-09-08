11bd968e064825620d9a11c9bc7db860498c950c
PolyakovSergey <ancientcult89@gmail.com>
2024-09-08 09:59:23 +0700

    page 180

---
+++ Marketplace.Domain/IClassifiedAdRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Marketplace.Domain
+{
+    public interface IClassifiedAdRepository
+    {
+        Task<bool> Exists(string id);
+
+        Task<ClassifiedAd> Load(string id);
+
+        Task Save(ClassifiedAd entity);
+    }
+}
---
+++ Marketplace/Api/ClassifiedAdsApplicationService.cs
@@ -0,0 +1,59 @@
+﻿using Marketplace.Contracts;
+using Marketplace.Domain;
+using Marketplace.Framework;
+using static Marketplace.Contracts.ClassifiedAds;
+
+namespace Marketplace.Api
+{
+    public class ClassifiedAdsApplicationService : IApplicationService
+    {
+        private readonly IClassifiedAdRepository _repository;
+        private ICurrencyLookUp _currencyLookUp;
+
+        public ClassifiedAdsApplicationService(IClassifiedAdRepository repository, ICurrencyLookUp currencyLookUp)
+        {
+            _currencyLookUp = currencyLookUp;
+            _repository = repository;
+        }
+
+        public Task Handle(object command) =>
+            command switch
+            {
+                V1.Create cmd =>
+                    HandleCreate(cmd),
+                V1.SetTitle cmd =>
+                    HandleUpdate(cmd.Id, c => c.SetTitle(ClassifiedAdTitle.FromString(cmd.Title))),
+                V1.UpdateText cmd =>
+                    HandleUpdate(cmd.Id, c => c.UpdateText(ClassifiedAdText.FromString(cmd.Text))),
+                V1.UpdatePrice cmd =>
+                    HandleUpdate(cmd.Id, c => c.UpdatePrice(Price.FromDecimal(cmd.Price, cmd.Currency, _currencyLookUp))),
+                V1.RequestToPublish cmd =>
+                    HandleUpdate(cmd.Id, c => c.RequestToPublish()),
+                _ => Task.CompletedTask
+            };
+        
+
+        private async Task HandleCreate(V1.Create cmd)
+        {
+            if (await _repository.Exists(cmd.Id.ToString()))
+                throw new InvalidOperationException($"Entity with id {cmd.Id} already exists");
+
+            var classifiedAd = new ClassifiedAd(
+                new ClassifiedAdId(cmd.Id),
+                new UserId(cmd.OwnerId)
+            );
+            await _repository.Save(classifiedAd);
+        }
+
+        private async Task HandleUpdate(Guid classifiedAdId, Action<ClassifiedAd> operation)
+        {
+            var classifiedAd = await _repository.Load(classifiedAdId.ToString());
+            if (classifiedAd == null)
+                throw new InvalidOperationException($"Entity with id {classifiedAdId} cannot be found");
+
+            operation(classifiedAd);
+
+            await _repository.Save(classifiedAd);
+        }
+    }
+}
