0831c2a7640d9bc51ab0c7deddadfb2628ec3d10
PolyakovSergey <ancientcult89@gmail.com>
2024-09-14 14:16:47 +0700

    page 319

---
+++ Marketplace.Framework/IAggregateStore.cs
@@ -0,0 +1,9 @@
+﻿namespace Marketplace.Framework
+{
+    public interface IAggregateStore
+    {
+        Task<bool> Exists<T, TId>(TId aggrefateId);
+        Task Save<T, TId>(T aggregate) where T : AggregateRoot<TId>;
+        Task Load<T, TId>(TId aggregateId) where T : AggregateRoot<TId>;
+    }
+}
